c4e6b47c44c17d9b933a6a2707bd1142717f6b5e
Mital-123 <mailto:mittalvaghela2003@gmail.com>
2025-05-21 17:46:49 +0530

    phone number link in footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IoMdPerson } from 'react-icons/io'
 import { LuPhoneCall } from 'react-icons/lu'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
@@ -17,8 +18,8 @@ function Footer() {
                             <div className='p-4 h-100 shadow custom-box' style={{ border: "4px double white" }}>
                                 <div className='fw-bold fs-4 infocolor'>PG Click Office</div>
                                 <div className='my-2'>
-                                    <div className='mb-2 footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>98259 99079</span></div>
-                                    <div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>76989 00398</span></div>
+                                    <Link to={"tel:98259 99079"} className='text-light text-decoration-none'><div className='mb-2 footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>98259 99079</span></div></Link>
+                                    <Link to={"tel:76989 00398"} className='text-light text-decoration-none'><div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>76989 00398</span></div></Link>
                                 </div>
                             </div>
                         </div>
@@ -28,7 +29,7 @@ function Footer() {
                                 <div className='fw-bold fs-4 infocolor'>Founder of PG Click</div>
                                 <div className='my-2'>
                                     <div className='mb-2 footer_no'><IoMdPerson className='text-warning footer_icon' /><span className='ms-2'>Lekha Gheewala</span></div>
-                                    <div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>94286 31038</span></div>
+                                    <Link to={"tel:94286 31038"} className='text-light text-decoration-none'><div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>94286 31038</span></div></Link>
                                 </div>
                             </div>
                         </div>
@@ -38,7 +39,7 @@ function Footer() {
                                 <div className='fw-bold fs-4 infocolor'>Event Planner</div>
                                 <div className='my-2'>
                                     <div className='mb-2 footer_no'><IoMdPerson className='text-warning footer_icon' /><span className='ms-2'>Kiran Sheth</span></div>
-                                    <div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>99794 60009</span></div>
+                                    <Link to={"tel:99794 60009"} className='text-light text-decoration-none'><div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>99794 60009</span></div></Link>
                                 </div>
                             </div>
                         </div>
