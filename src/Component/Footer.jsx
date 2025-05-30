645e0ab23d09376b129bf6a37f7c4d17c91461b4
tankvaishali <vaishalitank28603@gmail.com>
2025-05-30 13:37:40 +0530

    all changes done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -8,65 +8,12 @@ function Footer() {
     <>
       <div className='footer_bg py-3 py-lg-4 text-light text-center'>
         <div className='container'>
-
-          {/* <div className="row bglightbluecolor p-5 p-lg-4 rounded-4 d-flex justify-content-center rounded-pill  ">
-          
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0  pt-3 pt-lg-0 " data-aos="fade-right" data-aos-duration="1500" data-aos-once="true" data-aos-delay="50">
-              <div className="h-100">
-                <div className="text-center mb-4">
-                  <div className=" px-4 fs-4 rounded-4 fw-medium text-uppercase" style={{ display: "inline-block", background: "var(--info)" }}>
-                    Our Supporters
-                  </div>
-                </div>
-                <div className="row g-3 justify-content-center align-items-center">
-                  {[
-                    "qt=q_95.webp",
-                    "SUPPORTER 01.jpg",
-                    "SUPPORTER 02.jpg",
-                    "SUPPORTER 03.jpg",
-                  ].map((img, index) => (
-                    <div key={index} className="col-6 col-md-3 d-flex justify-content-center" >
-                      <div className="supporter-img-wrapper">
-                        <img
-                          src={require(`../assets/eximagies/${img}`)}
-                          alt={`Supporter ${index + 1}`}
-                          className="img-fluid supporter-img"
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-
-  
-            <div className="col-12 col-lg-4" data-aos="fade-left" data-aos-duration="1500" data-aos-once="true" data-aos-delay="50">
-              <div className="h-100">
-                <div className="text-center mb-4">
-                  <div className=" px-4 fs-4 rounded-4 fw-medium text-uppercase" style={{ display: "inline-block", background: "var(--info)" }}>
-                    Photography by
-                  </div>
-                </div>
-                <div className="d-flex justify-content-center">
-                  <div className="photography-img-wrapper">
-                    <img
-                      src={require("../assets/eximagies/photography.jpg")}
-                      alt="Photography by"
-                      className="img-fluid supporter-img"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
-
-
           <div className='pb-4  mt-2 mt-lg-4 d-flex justify-content-center'>
             <div className="py-2 px-3 fs-4 rounded-4 fw-medium text-uppercase" style={{ width: "fit-content", background: "var(--info)" }} data-aos="fade-up" data-aos-duration="1500" data-aos-once="true" >
               Stall Bookings Open – Contact Now!
             </div>
           </div>
-          <div className='row g-4 p-2 p-lg-0'>
+          <div className='row justify-content-center g-4 p-2 p-lg-0'>
             <div className='col-12 col-lg-4' data-aos="fade-down" data-aos-duration="1500" data-aos-once="true" data-aos-delay="50">
               <div className='p-4 h-100 shadow custom-box' style={{ border: "4px double white" }}>
                 <div className='fw-bold fs-4 infocolor'>PG Click Office</div>
@@ -76,7 +23,7 @@ function Footer() {
                 </div>
               </div>
             </div>
-
+            {/* 
             <div className='col-12 col-lg-4' data-aos="fade-down" data-aos-duration="1500" data-aos-once="true" data-aos-delay="250">
               <div className='p-4 h-100 shadow custom-box' style={{ border: "4px double white" }}>
                 <div className='fw-bold fs-4 infocolor'>Founder of PG Click</div>
@@ -85,7 +32,7 @@ function Footer() {
                   <Link to={"tel:94286 31038"} className='text-light text-decoration-none'><div className='footer_no'><LuPhoneCall className='text-warning footer_icon' /><span className='ms-2'>94286 31038</span></div></Link>
                 </div>
               </div>
-            </div>
+            </div> */}
 
             <div className='col-12 col-lg-4' data-aos="fade-down" data-aos-duration="1500" data-aos-once="true" data-aos-delay="450">
               <div className='p-4 h-100 shadow custom-box' style={{ border: "4px double white" }}>
--- src/Component/Schedule.jsx
+++ src/Component/Schedule.jsx
@@ -1,174 +1,174 @@
 import React from 'react'
 
 function Schedule() {
-    return (
-        <>
-            <section class="schedule overflow-hidden">
-                <div class="container">
-                    <div className="col-lg-10 mx-auto day-schedule">
-                        <div class="">
-                            <div className="day-header">
-                                <div className="day-number">21</div>
-                                <div className="day-info">
-                                    <h3>Day 1</h3>
-                                    <p>Saturday, June 2025</p>
-                                </div>
-                            </div>
-                            {/* <h2 className='fw-bold'>21st June Events</h2> */}
-                            <ul class="event-list ps-0" style={{ fontFamily: "Poppins" }}>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="50">
-                                    <span class="time">10:00 am</span>
-                                    <span class="event-name">Inauguration</span>
-                                </li>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="250">
-                                    <span class="time">2:30 pm</span>
-                                    <span class="event-name">Healthy Cooking Show (Chef Anju Srivastava)</span>
-                                </li>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="450">
-                                    <span class="time">5:00 pm</span>
-                                    <span class="event-name">Award Ceremony (Star Celebrities of Surat)</span>
-                                </li>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="650">
-                                    <span class="time">7:00 pm</span>
-                                    <span class="event-name">Yoga Day Celebration</span>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="mt-4">
-                            <div className="day-header">
-                                <div className="day-number">22</div>
-                                <div className="day-info">
-                                    <h3>Day 2</h3>
-                                    <p>Sunday, June 2025</p>
-                                </div>
-                            </div>
-                            {/* <h2 className='fw-bold'>22nd June Events</h2> */}
-                            <ul class="event-list ps-0">
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="50">
-                                    <span class="time">11:30 am</span>
-                                    <span class="event-name">Adv. Priti Joshi (Learning Session)</span>
-                                </li>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="250">
-                                    <span class="time">2:00 pm</span>
-                                    <span class="event-name">Lekha Gheewala (Business Fun Activities)</span>
-                                </li>
-                                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="450">
-                                    <span class="time">4:00 pm</span>
-                                    <span class="event-name">Mega Housiee</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section class="schedule overflow-hidden">
+        <div class="container">
+          <div className="col-lg-10 mx-auto day-schedule">
+            <div class="">
+              <div className="day-header">
+                <div className="day-number">21</div>
+                <div className="day-info">
+                  <h3>Day 1</h3>
+                  <p>Saturday, June 2025</p>
                 </div>
+              </div>
+              {/* <h2 className='fw-bold'>21st June Events</h2> */}
+              <ul class="event-list ps-0" style={{ fontFamily: "Poppins" }}>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="50">
+                  <span class="time">11:00 am</span>
+                  <span class="event-name">Inauguration</span>
+                </li>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="250">
+                  <span class="time">2:30 pm</span>
+                  <span class="event-name">Healthy Cooking Show (Chef Anju Srivastava)</span>
+                </li>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="450">
+                  <span class="time">4:30 pm</span>
+                  <span class="event-name">Award Ceremony (Star Celebrities of Surat)</span>
+                </li>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="650">
+                  <span class="time">6:00 pm</span>
+                  <span class="event-name">Yoga Day Celebration (Mrs.Rimpal Jariwala)</span>
+                </li>
+              </ul>
+            </div>
 
-              <div className="marquee-wrapper" >
-  <div className="marquee">
-    <div className="marquee-content">
-      {[
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-      ].map((text, i) => (
-        <span
-          key={i}
-          className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
-        >
-          {text}
-        </span>
-      ))}
+            <div class="mt-4">
+              <div className="day-header">
+                <div className="day-number">22</div>
+                <div className="day-info">
+                  <h3>Day 2</h3>
+                  <p>Sunday, June 2025</p>
+                </div>
+              </div>
+              {/* <h2 className='fw-bold'>22nd June Events</h2> */}
+              <ul class="event-list ps-0">
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="50">
+                  <span class="time">11:30 am</span>
+                  <span class="event-name">Tr.Lekha Gheewala (Business Activities)</span>
+                </li>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="250">
+                  <span class="time">3:00 pm</span>
+                  <span class="event-name">Adv. priti Joshi (Free Legal Camp)</span>
+                </li>
+                <li data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="450">
+                  <span class="time">4:00 pm</span>
+                  <span class="event-name">Mega Housie (Anchor Kiran Sheth)</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-      {/* Duplicate for seamless scroll */}
-      {[
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-      ].map((text, i) => (
-        <span
-          key={`dup-${i}`}
-          className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
-        >
-          {text}
-        </span>
-      ))}
-      {[
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-      ].map((text, i) => (
-        <span
-          key={`dup-${i}`}
-          className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
-        >
-          {text}
-        </span>
-      ))}
-      {[
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-      ].map((text, i) => (
-        <span
-          key={`dup-${i}`}
-          className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
-        >
-          {text}
-        </span>
-      ))}
-      {[
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-        'Businessotsav Exhibition 2025 🔊',
-        'Free Entry For Visitors.',
-      ].map((text, i) => (
-        <span
-          key={`dup-${i}`}
-          className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
-        >
-          {text}
-        </span>
-      ))}
-    </div>
-  </div>
-</div>
+        <div className="marquee-wrapper" >
+          <div className="marquee">
+            <div className="marquee-content">
+              {[
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+              ].map((text, i) => (
+                <span
+                  key={i}
+                  className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
+                >
+                  {text}
+                </span>
+              ))}
 
-            </section>
-        </>
-    )
+              {/* Duplicate for seamless scroll */}
+              {[
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+              ].map((text, i) => (
+                <span
+                  key={`dup-${i}`}
+                  className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
+                >
+                  {text}
+                </span>
+              ))}
+              {[
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+              ].map((text, i) => (
+                <span
+                  key={`dup-${i}`}
+                  className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
+                >
+                  {text}
+                </span>
+              ))}
+              {[
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+              ].map((text, i) => (
+                <span
+                  key={`dup-${i}`}
+                  className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
+                >
+                  {text}
+                </span>
+              ))}
+              {[
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+                'Businessotsav Exhibition 2025 🔊',
+                'Free Entry For Visitors.',
+              ].map((text, i) => (
+                <span
+                  key={`dup-${i}`}
+                  className={`font_stroke me-4 ${text.includes('Free') ? 'text-danger' : ''}`}
+                >
+                  {text}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </>
+  )
 }
 
 export default Schedule
